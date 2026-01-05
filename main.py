af2ca1d28b8df5743c7cba6312c0086ad41ca394
Gupples <gupples224@gmail.com>
2026-01-05 15:35:24 -0800

    Included tracking of AI tokens

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ def main():
         raise RuntimeError("API KEY NOT FOUND!!!")
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(model="gemini-2.5-flash", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    if response.usage_metadata == None:
+        raise RuntimeError("No usage_metadata!!")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print(response.text)
 
 
