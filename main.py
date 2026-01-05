d6b4e76d859274cf438f1bbc319fd75271a57e5c
Gupples <gupples224@gmail.com>
2026-01-05 15:45:28 -0800

    Enabled user prompts

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import argparse
 
 def main():
     print("Hello from ai-handiman!")
+
+    # Set up LLM
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     if api_key == None:
         raise RuntimeError("API KEY NOT FOUND!!!")
     client = genai.Client(api_key=api_key)
-    response = client.models.generate_content(model="gemini-2.5-flash", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    
+    # Set up args.user_prompt
+    parser =  argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args  = parser.parse_args()
+
+    # Process user prompt
+    response = client.models.generate_content(model="gemini-2.5-flash", contents=args.user_prompt)
+
+    # Display resutls
     if response.usage_metadata == None:
         raise RuntimeError("No usage_metadata!!")
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
