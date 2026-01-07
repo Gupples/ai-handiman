c7fc8bd8096db8b74a6f438a04a6a9e972ca0bd2
Gupples <gupples224@gmail.com>
2026-01-06 18:32:51 -0800

    Added print sections for test separation clarity

--- test_run_python_file.py
+++ test_run_python_file.py
@@ -1,19 +1,31 @@
 from functions.run_python_file import run_python_file
 
 # Print calculator's usage instructions
+print("@" * 25 )
 results = run_python_file("calculator", "main.py")
+print(results)
 
 # Run the calculator
+print("@" * 25 )
 results = run_python_file("calculator", "main.py", ["3 + 5"])
+print(results)
 
 # Run the calculator's tests successfully
+print("@" * 25 )
 results = run_python_file("calculator", "tests.py")
+print(results)
 
 # Error: Out of working directory
+print("@" * 25 )
 results = run_python_file("calculator", "../main.py")
+print(results)
 
 # Error: Doesn't exist
+print("@" * 25 )
 results = run_python_file("calculator", "nonexistent.py") 
+print(results)
 
 # Error: Not a Python file
+print("@" * 25 )
 results = run_python_file("calculator", "lorem.txt")
+print(results)
