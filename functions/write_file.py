27e951d4b1c9c3467de7dec038c04775a9d59600
Gupples <gupples224@gmail.com>
2026-01-06 16:51:51 -0800

    Created write_file.py, added basic checks for permitted directories and .isfile()

---
+++ functions/write_file.py
@@ -0,0 +1,16 @@
+import os
+
+def write_file(working_directory, file_path, content):
+    try:
+        working_dir_abs = os.path.abspath(working_directory)
+        target_file = os.path.normpath(os.path.join(working_dir_abs, file_path))
+        valid_target_file = os.path.commonpath([working_dir_abs, target_file]) == working_dir_abs
+
+        if not valid_target_file:
+            return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
+        if not os.path.isfile(target_file):
+            return f'Error: Cannot write to "{file_path}" as it is a directory'
+        
+        
+    except Exception as e:
+        return f"Error writing files: {e}"
