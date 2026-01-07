cf3c763ac882f193b03cc8107d8218182d386ae6
Gupples <gupples224@gmail.com>
2026-01-06 19:24:25 -0800

    Updated list of available functions and ensured the agent knows how to request that they be called

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from dotenv import load_dotenv
 from google import genai
 import argparse
 from google.genai import types
+from functions.get_file_content import schema_get_file_content
+from functions.get_files_info import schema_get_files_info
+from functions.run_python_file import schema_run_python_file
+from functions.write_file import schema_write_file
 
 def main():
 
@@ -12,6 +16,22 @@ def main():
     if api_key == None:
         raise RuntimeError("API KEY NOT FOUND!!!")
     client = genai.Client(api_key=api_key)
+    system_prompt = """
+    You are a helpful AI coding agent.
+
+    When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+    - List files and directories
+
+    All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+    """ 
+
+    available_functions = types.Tool(
+        function_declarations=[schema_get_files_info, 
+                               schema_get_file_content, 
+                               schema_run_python_file, 
+                               schema_write_file],
+    )
     
     # Set up args
     parser =  argparse.ArgumentParser(description="Chatbot")
@@ -21,7 +41,13 @@ def main():
 
     # Process user prompt
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
-    response = client.models.generate_content(model="gemini-2.5-flash", contents=messages)
+    response = client.models.generate_content(
+        model="gemini-2.5-flash", 
+        contents=messages,
+        config=types.GenerateContentConfig(
+            tools=[available_functions], 
+            system_instruction=system_prompt),
+        )
 
     # Display resutls
     if response.usage_metadata == None:
@@ -30,6 +56,9 @@ def main():
         print(f"User prompt: {args.user_prompt}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if len(response.function_calls) != 0:
+        for function_call in response.function_calls:
+            print(f"Calling function: {function_call.name}({function_call.args})")
     print(response.text)
 
 
