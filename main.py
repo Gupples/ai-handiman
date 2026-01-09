f1f447d9295350033ae4f744743eb6f7cb187e4f
Gupples <gupples224@gmail.com>
2026-01-09 13:31:01 -0800

    Made system prompt data-driven

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 from google import genai
 import argparse
 from google.genai import types
+from prompts import SYSTEM_PROMPT
 from functions.get_file_content import schema_get_file_content, get_file_content
 from functions.get_files_info import schema_get_files_info, get_files_info
 from functions.run_python_file import schema_run_python_file, run_python_file
@@ -16,18 +17,7 @@ def main():
     if api_key == None:
         raise RuntimeError("API KEY NOT FOUND!!!")
     client = genai.Client(api_key=api_key)
-    system_prompt = """
-    You are a helpful AI coding agent.
-
-    When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
-
-    - List files and directories
-    - Read file contents
-    - Execute Python files with optional arguments
-    - Write or overwrite files
-
-    All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
-    """ 
+    system_prompt = SYSTEM_PROMPT
 
     available_functions = types.Tool(
         function_declarations=[schema_get_files_info, 
